ccad0d4029da196ad5226825eedd3c562f3cc838
Ilya <techno19.01.91@gmail.com>
2021-01-23 14:42:45 +0600

    Объекты, деструктуризация объектов

---
+++ 020 Объекты, деструктуризация объектов (ES6)/js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+
+const { border, bg} = options.colors;
+console.log(bg);
+
+
+
+// const girl = {
+//     name: 'Mary',
+//     age: 26,
+//     isMarried: false,
+//     hasSister: true
+// };
+
+// //console.log(options.name);
+
+// // delete options.name;
+// // console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof(options[key]) === 'object') {
+//         for(let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             //counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(Object.keys(options).length);
+
+// console.log(Object.keys(girl));
+
+
+//console.log(counter);
+
